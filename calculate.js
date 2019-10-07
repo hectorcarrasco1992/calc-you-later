4fdc3dc9f910a1f6ba5a3dba8cb80dcf87a1ff10
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-07 13:49:00 -0400

    finished test

--- calculate.js
+++ calculate.js
@@ -1,6 +1,53 @@
-function calculate() {
-  
-}
+function calculate(num,num1,operation) {
+const realnum = Number(num)
+const realnum1 =Number(num1)
+let answer = realnum + realnum1
+
+if (operation === '+'){
+    answer = realnum + realnum1
+}else if (operation === '-'){
+    answer = realnum - realnum1
+}else if (operation === 'x'){
+    answer = realnum * realnum1
+}else if (operation === '/'){
+    answer = realnum / realnum1
+}else if (operation === '%'){
+    answer = realnum % realnum1
+}else if (operation === 'plus'){
+    answer = realnum + realnum1
+}else if (operation === 'minus'){
+    answer = realnum - realnum1
+}else if (operation === 'times'){
+    answer = realnum * realnum1
+}else if (operation === 'X'){
+    answer = realnum * realnum1
+}else if (operation === 'modulus'){
+    answer = realnum % realnum1
+}else if ( operation === 'mod'){
+    answer = realnum % realnum1
+}else answer = "Sorry, that's not a mathematical operation!"
+
+
+return answer
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = calculate;
